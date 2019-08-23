7fa92b043897f9447e2082dafec775ac104ec8f0
Feross Aboukhadijeh <feross@feross.org>
2019-08-23 15:31:53 -0700

    ts: update definition to add encoding
    cc @ryansmith94 - can you confirm this is the correct way to specify an optional encoding parameter?

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module 'string-to-stream' {
-  const stringToStream: (data: string) => NodeJS.ReadableStream;
+  const stringToStream: (data: string, encoding: string?) => NodeJS.ReadableStream;
   export = stringToStream;
 }
